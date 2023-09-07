af1215cad65f5fa792e28cb5ddab5491a56a4524
JoacoSagasta <joacosagasta@hotmail.com>
2023-09-07 19:19:21 -0300

    subo correccion de carpetas

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
+import { resolve } from 'node:path'
+
+console.log(resolve('views')) /*quiero saber la ruta completa(absoluta) donde se encuentra la carpeta "view"*/
+
+
 export default{
     server: {
         port: '2222',
     },
     css: {
         devSourcemap: true
+    },
+    build: {
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                alta: resolve('views/alta.html'),
+                carrito: resolve('views/carrito.html'),
+                contacto: resolve('views/contacto.html'),
+                nosotros: resolve('views/nosotros.html'),
+                inicio: resolve('views/index.html')
+            }
+        }
     }
 }
